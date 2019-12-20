1dd3032b61bcebda58403ec3be9bb51bf30fffed
yangzong18 <183851063@qq.com>
2019-12-20 18:07:01 +0800

    第一个vue 项目

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Index from '../views/index/index'
+import Manage from '../views/manage/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/manage',
+      name: 'Manage',
+      component: Manage
+    }
+  ]
+})
---
+++ src/views/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+      欢迎来到人员管理
+    <!-- <h1>{{ msg }}</h1> -->
+    <footer-nav v-bind:class="{'isIndex':isNowPage}"></footer-nav>
+  </div>
+</template>
+
+<script>
+import footer from '../../components/footer.vue'
+export default {
+    name:'Index',
+    components: {
+        'footer-nav':footer
+    },
+    data() {
+        return {
+            isNowPage:true
+        }
+    },
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
